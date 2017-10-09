56de0f6a55e511f38d1d5a6fd2c07f9204ea2e24
Jason Lin <uscjlin@gmail.com>
2017-10-09 16:16:49 -0400

    solved permute parens

--- ch8/p9.py
+++ ch8/p9.py
@@ -4,3 +4,33 @@ EXAMPLE
 Input: 3
 Output: ((())), (()()), (())(), ()(()), ()()()
 """
+
+def permParens(n):
+	perms = []
+	genParens(perms, n, n, [0]*2*n, 0)
+	return perms
+
+def genParens(perms, leftrem, rightrem, parenBuilder, idx):
+
+	if leftrem < 0 or rightrem < leftrem: 
+		return
+
+	if leftrem == 0 and rightrem == 0:
+		parenStr = "".join(parenBuilder)
+		perms.append(parenStr)
+	else:
+		# parenBuilder.append("(")
+		parenBuilder[idx] = "("
+		genParens(perms, leftrem-1, rightrem, parenBuilder, idx+1)
+
+		# parenBuilder.append("(")
+		parenBuilder[idx] = ")"
+		# parenBuilder[-1] = ")"
+		genParens(perms, leftrem, rightrem-1, parenBuilder, idx+1)
+
+
+# Test cases
+print "n=1,", permParens(1)
+print "n=2,", permParens(2)
+print "n=3,", permParens(3)
+# print "n=21,", permParens(6)	# check: (()(()))()() is valid

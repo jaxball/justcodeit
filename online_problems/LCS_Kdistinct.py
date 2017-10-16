a5ac8c3e1e90fc4ef9c36876276d00429fe2cba1
Jason Lin <uscjlin@gmail.com>
2017-10-16 01:16:57 -0400

    correct solution for longest consecutive substring of 2 distinct characters

--- online_problems/LCS_Kdistinct.py
+++ online_problems/LCS_Kdistinct.py
@@ -20,17 +20,17 @@ return: bebebe
 
 """
 
-def searchLeftRight(left, right, beg, curr, line, res):
-    while left > 0: 
-        if line[left] == beg or line[left] == curr:
-            res.insert(0, line[left])
+def searchLeftRight(left, right, beg, end, line, window):
+    while left >= 0: 
+        if line[left] == beg or line[left] == end:
+            window.insert(0, line[left])
         else:
             break
         left -= 1
 
     while right < len(line)-1: 
-        if line[right] == beg or line[right] == curr:
-            res.append(line[right])
+        if line[right] == beg or line[right] == end:
+            window.append(line[right])
         else:
             break
         right += 1
@@ -41,71 +41,38 @@ def LCSdistinct(line):
         return ""
 
     maxString = []
-    res = []
-    beg = ""
-    curr = ""
-    maxLen = 0
-    # beg = line[0]
-    # i = 1
-    # res.append(beg)
+    window = []
+    beg = "" 
+    end = ""
 
-    # left = 0
-    # right = 0 
-
-    # currLen = 0
-    # while line[i] == beg and i < len(line)-1:
-    #     currLen += 1
-    #     i += 1
-    # curr = line[i]
-    # res.append(curr)
-
-
-    for i, c in enumerate(line[2:]):
-        # print "beginnig of loop", c, beg, curr
-        if c == beg or c == curr:
-            res.append(c)
+    for i, c in enumerate(line[:]):
+        if c == beg or c == end:
+            window.append(c)
             continue
         else: 
-            right = i + 2
-            left = right - len(res) - 1
-            
-            searchLeftRight(left, right, beg, curr, line, res)
-            # while left > 0: 
-            #     if line[left] == beg or line[left] == curr:
-            #         res.insert(0, line[left])
-            #         currLen += 1
-            #     else:
-            #         break
-            #     left -= 1
+            right = i 
+            left = right - len(window) - 1            
+            searchLeftRight(left, right, beg, end, line, window)
 
-            # while right < len(line)-1: 
-            #     if line[right] == beg or line[right] == curr:
-            #         res.append(line[right])
-            #         currLen += 1
-            #     else:
-            #         break
-            #     right += 1
+            maxLen = max(len(maxString), len(window))
+            if maxLen == len(window):
+                maxString = window
 
-            maxLen = max(maxLen, len(res))
-            if maxLen == len(res):
-                maxString = res
+            window = []            
+            beg = line[i-1]
+            end = c
 
-            res = []            
-            beg = line[i+2-1]
-            curr = c
-            # res.append(beg)
-            # res.append(curr)
+    # Handles the case when string ends with the longest desired substring
+    # right = i; left = right - len(window) 
+    searchLeftRight(right, right+len(window), beg, end, line, window)
 
-    right = i + 2
-    left = right - len(res) 
-    searchLeftRight(left, right, beg, curr, line, res)
-
-    currLen = len(res)
-    maxLen = max(maxLen, currLen)
-    if maxLen == currLen:
-        maxString = res
-
+    maxLen = max(maxLen, len(window))
+    if maxLen == len(window):
+        maxString = window
     return ''.join(maxString)
+
+
+# Test cases
 
 s1 = "aabacbebebe"
 s1b = "aabacbebebea"
@@ -120,3 +87,9 @@ print "s2 =", s2, "output =", LCSdistinct(s2)
 print "s3 =", s3, "output =", LCSdistinct(s3)
 print "s4 =", s4, "output =", LCSdistinct(s4)
 print "s5 =", s4, "output =", LCSdistinct(s5)
+
+assert LCSdistinct(s1) == "bebebe"
+assert LCSdistinct(s2) == "abbaa", "Error on input = " + s2  + " (" + LCSdistinct(s2) + "). expected: abbaa"
+assert LCSdistinct(s3) == "abbabaab"
+assert LCSdistinct(s4) == "ddddcdcc"
+assert LCSdistinct(s5) == "bbcbc"

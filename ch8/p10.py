b9d8b22a5e1d068a5fbb70f5de79dc136fa5e17d
Jason Lin <uscjlin@gmail.com>
2017-10-16 01:18:04 -0400

    attempted ch8 p10-11

--- ch8/p10.py
+++ ch8/p10.py
@@ -1,3 +1,34 @@
 """
 Paint Fill: Implement the "paint fill" function that one might see on many image editing programs. That is, given a screen (represented by a two-dimensional array of colors), a point, and a new color, fill in the surrounding area until the color changes from the original color. 
 """
+from enum import Enum
+
+
+class Color(Enum):
+	Black = 1
+	White = 2
+	Red = 3
+	Yellow = 4
+	Green = 5
+
+def paintFill(image, x, y, newC):
+	currColor = image[x][y]
+	if currColor == newC: 
+		return False
+	return paintFillHelper(image, x, y, newC, currColor)
+
+def paintFillHelper(image, x, y , newC, oldC):
+
+	if y >= len(image) or y < 0 or x >= len(image[0]) or x < 0:
+		return False
+
+	currColor = image[x][y]
+	if currColor == oldC:
+	   image[x][y] = newC
+	   paintFillHelper(image, x-1, y, newC, oldC)
+	   paintFillHelper(image, x+1, y, newC, oldC)
+	   paintFillHelper(image, x, y-1, newC, oldC)
+	   paintFillHelper(image, x, y+1, newC, oldC)
+
+	return True
+
--- ch8/p11.py
+++ ch8/p11.py
@@ -1,3 +1,8 @@
 """
 Coins; Given an infinite number of quarters (25 cents), dimens (10 cents), nickels (5 cents), and pennies (1 cent), write code to calculate the number of ways of representing n cents.
 """
+
+""" Solved on paper youself, left as exercise =)
+	remember to count the number of WAYS of representation and not number of COINS used 
+	for each combination. 
+"""
